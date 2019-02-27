91b89a9182d21b8d8d2b758946e3932340b0333d
neerajankam <32281467+neerajankam@users.noreply.github.com>
2019-02-27 14:19:47 -0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,16 +1,22 @@
 class Stack:
 	def __init__(self):
 		self.elems=[]
+		
 	def push(self,val):
 	    return self.elems.append(val)
+	
 	def pop(self):
 	    return self.elems.pop()
+	
 	def isEmpty(self):
 	    return self.elems == []
+	
 	def peek(self):
 	    return self.elems[len(self.elems)-1]
+	
 	def size(self):
-	    return len(self.elems)    
+	    return len(self.elems)   
+
 s=Stack()
 s.push(1)
 s.push(123)
@@ -21,7 +27,6 @@ s.push(14)
 s.pop()
 print(s.isEmpty())
 print(s.peek())
-
 s.pop()
 print(s.peek())
 
